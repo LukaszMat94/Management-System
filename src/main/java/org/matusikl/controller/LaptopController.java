8f67b712ee839848fecae0206234c430165b3f3b
LukaszMat94 <lmatusik11@gmail.com>
2021-12-15 13:13:07 +0100

    Updated to LaptopController
    Initial LaptopController, added CRUD operations, changed to EntityResponse

---
+++ src/main/java/org/matusikl/controller/LaptopController.java
@@ -0,0 +1,53 @@
+package org.matusikl.controller;
+
+import org.matusikl.model.Laptop;
+import org.matusikl.service.LaptopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class LaptopController {
+
+    LaptopService laptopService;
+
+    @Autowired
+    public LaptopController(LaptopService laptopService){
+        this.laptopService = laptopService;
+    }
+
+    @GetMapping(value = "/laptops", produces = "application/json")
+    public ResponseEntity<List<Laptop>> getLaptops(){
+
+        return new ResponseEntity<>(laptopService.getLaptops(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/laptop", produces = "application/json")
+    public ResponseEntity<Laptop> getLaptop(){
+
+        return new ResponseEntity<>(laptopService.getLaptop(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/add/laptop", consumes = "application/json", produces = "text/plain")
+    public ResponseEntity<String> addLaptop(@RequestBody Laptop laptop){
+        laptopService.addLaptop(laptop);
+        return new ResponseEntity<>("Laptop added ", HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/delete/laptop/{id}", consumes = "application/json", produces = "text/plain")
+    public ResponseEntity<String> deleteLaptop(@PathVariable ("id") Integer idLaptop){
+        laptopService.deleteLaptop(idLaptop);
+        return new ResponseEntity<>("Laptop deleted ", HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/update/laptop/{id}")
+    public ResponseEntity<Laptop> updateLaptop(@PathVariable ("id") Integer idLaptop,
+                                               @Valid @RequestBody Laptop laptop){
+        laptopService.updateLaptop(idLaptop, laptop);
+        return new ResponseEntity<>(laptop, HttpStatus.OK);
+    }
+}
