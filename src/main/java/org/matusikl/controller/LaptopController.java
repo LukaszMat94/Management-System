6aef1cbe97b06e993fda52954d8fefc889a51dfe
LukaszMat94 <lmatusik11@gmail.com>
2021-12-16 20:05:21 +0100

    Updated LaptopController class
    Configured procudes, consumes MediaType, correctly communicate with service and handling exceptions, without validate parameters on request

--- src/main/java/org/matusikl/controller/LaptopController.java
+++ src/main/java/org/matusikl/controller/LaptopController.java
@@ -1,13 +1,12 @@
 package org.matusikl.controller;
 
-import org.matusikl.exception.DataNotFoundException;
 import org.matusikl.model.Laptop;
 import org.matusikl.service.LaptopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -20,34 +19,46 @@ public class LaptopController {
         this.laptopService = laptopService;
     }
 
-    @GetMapping(value = "/laptops", produces = "application/json")
+    @GetMapping(value = "/get/laptops", produces = "application/json")
     public ResponseEntity<List<Laptop>> getLaptops() {
-        return new ResponseEntity<>(laptopService.getLaptops(), HttpStatus.OK);
+        return ResponseEntity
+                .ok()
+                .body(laptopService.getLaptops());
     }
 
-    @GetMapping(value = "/laptop/{id}", produces = "application/json")
+    @GetMapping(value = "/get/laptop/{id}", produces = "application/json")
     public ResponseEntity<Laptop> getLaptop(@PathVariable("id") Integer id){
 
-        return new ResponseEntity<>(laptopService.getLaptop(id), HttpStatus.OK);
+        return ResponseEntity
+                .ok()
+                .body(laptopService.getLaptop(id));
     }
 
-    @PostMapping(value = "/add/laptop", consumes = "application/json", produces = "text/plain")
+    @PostMapping(value = "/add/laptop", consumes = "application/json", produces = {"text/plain", "application/json"})
     public ResponseEntity<String> addLaptop(@RequestBody Laptop laptop){
         laptopService.addLaptop(laptop);
-        return new ResponseEntity<>("Laptop added ", HttpStatus.OK);
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body("Laptop added");
     }
 
-    @DeleteMapping(value = "/delete/laptop/{id}", consumes = "application/json", produces = "text/plain")
+    @DeleteMapping(value = "/delete/laptop/{id}", consumes = "application/json", produces = {"text/plain", "application/json"})
     public ResponseEntity<String> deleteLaptop(@PathVariable ("id") Integer idLaptop){
         laptopService.deleteLaptop(idLaptop);
-        return new ResponseEntity<>("Laptop deleted ", HttpStatus.OK);
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body("Laptop deleted");
     }
 
-    @PutMapping(value = "/update/laptop/{id}")
+    @PutMapping(value = "/update/laptop/{id}", produces = {"text/plain", "application/json"})
     public ResponseEntity<Laptop> updateLaptop(@PathVariable ("id") Integer idLaptop,
-                                               @Valid @RequestBody Laptop laptop){
-        laptopService.updateLaptop(idLaptop, laptop);
-        return new ResponseEntity<>(laptop, HttpStatus.OK);
+                                               @RequestBody Laptop laptop){
+        Laptop updatedLaptop = laptopService.updateLaptop(idLaptop, laptop);
+        return ResponseEntity
+                .ok()
+                .body(updatedLaptop);
     }
 
 }
