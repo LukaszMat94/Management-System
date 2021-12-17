1626629090a2a0f201eea3a46328dfdbecb147d4
LukaszMat94 <lmatusik11@gmail.com>
2021-12-17 22:21:47 +0100

    Updated ControllerAdvice class
    Changed from Map<String, Object> to ResponseEntity<Object> response handler exception

--- src/main/java/org/matusikl/controlleradvice/GlobalControllerAdvice.java
+++ src/main/java/org/matusikl/controlleradvice/GlobalControllerAdvice.java
@@ -1,47 +1,39 @@
 package org.matusikl.controlleradvice;
 
-import com.alibaba.fastjson.JSON;
 import org.matusikl.errorresponse.ErrorResponse;
 import org.matusikl.exception.DataDuplicateException;
 import org.matusikl.exception.DataNotFoundException;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import java.time.LocalDateTime;
-import java.util.Map;
+import java.time.ZonedDateTime;
 
-@EnableWebMvc
 @RestControllerAdvice
-@ComponentScan(basePackages = {"org.matusikl.errorresponse", "org.matusikl.controlleradvice", "org.matusikl.config"})
 public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(DataNotFoundException.class)
-    public Map<String, Object> handleDataNotFoundException(DataNotFoundException exception){
-
-            ErrorResponse errorResponse = new ErrorResponse();
-            errorResponse.setMessage(exception.getMessage());
-            errorResponse.setTime(LocalDateTime.now());
-            errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-
-        return getJsonMap(errorResponse);
-    }
-
-    @ExceptionHandler(DataDuplicateException.class)
-    public Map<String, Object> handleDataDuplicateException(DataDuplicateException exception){
+    public ResponseEntity<Object> handleDataNotFoundException(DataNotFoundException exception){
 
         ErrorResponse errorResponse = new ErrorResponse();
         errorResponse.setMessage(exception.getMessage());
-        errorResponse.setTime(LocalDateTime.now());
-        errorResponse.setStatus(HttpStatus.CONFLICT.value());
-
-        return getJsonMap(errorResponse);
+        errorResponse.setDateTime(ZonedDateTime.now());
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
-    public Map<String, Object> getJsonMap(ErrorResponse errorResponse){
-        return JSON.parseObject(JSON.toJSONString(errorResponse), Map.class);
+    @ExceptionHandler(DataDuplicateException.class)
+    public ResponseEntity<Object> handleDataDuplicateException(DataDuplicateException exception){
+
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage(exception.getMessage());
+        errorResponse.setDateTime(ZonedDateTime.now());
+        errorResponse.setStatus(HttpStatus.CONFLICT.value());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
 }
+
+
+
