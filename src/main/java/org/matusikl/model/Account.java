fa6602e45b1c0833636d364f7f81e4502c8b10db
LukaszMat94 <lmatusik11@gmail.com>
2021-12-18 15:11:12 +0100

    Updated Account class
    Prepared for Entity and CRUD operations with handling validation

--- src/main/java/org/matusikl/model/Account.java
+++ src/main/java/org/matusikl/model/Account.java
@@ -1,7 +1,20 @@
 package org.matusikl.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "MS_Account")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idAccount")
+    private Integer idAccount;
+    @Size(min = 8, message = "{account.login.size}")
+    @NotNull(message = "{account.login.null}")
     private String login;
+    @Size(min = 8, message = "{account.password.size}")
     private String password;
 
     public Account() {
@@ -23,5 +36,14 @@ public class Account {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public Integer getIdAccount() {
+        return idAccount;
+    }
+
+    public void setIdAccount(Integer idAccount) {
+        this.idAccount = idAccount;
+    }
+
     //endregion
 }
