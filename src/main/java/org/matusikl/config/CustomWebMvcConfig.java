cd7ba2d2c310be85115de9e79caf4e9e4a32e3cc
LukaszMat94 <lmatusik11@gmail.com>
2021-12-18 12:00:29 +0100

    Updated WebMvcConfig
    Visual changes

--- src/main/java/org/matusikl/config/CustomWebMvcConfig.java
+++ src/main/java/org/matusikl/config/CustomWebMvcConfig.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
