1b67121ec45f6005f0edc6a5f89a43c52d53d8dc
LukaszMat94 <lmatusik11@gmail.com>
2021-12-18 11:59:21 +0100

    Updated ControllerAdvice
    Override exception handler for validations errors (@Valid)

--- src/main/java/org/matusikl/controlleradvice/GlobalControllerAdvice.java
+++ src/main/java/org/matusikl/controlleradvice/GlobalControllerAdvice.java
@@ -3,12 +3,18 @@ package org.matusikl.controlleradvice;
 import org.matusikl.errorresponse.ErrorResponse;
 import org.matusikl.exception.DataDuplicateException;
 import org.matusikl.exception.DataNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
@@ -33,6 +39,17 @@ public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<ErrorResponse> errorList = new ArrayList<>();
+        for(ObjectError error : ex.getBindingResult().getAllErrors()){
+            errorList.add(new ErrorResponse(
+                    error.getDefaultMessage(),
+                    HttpStatus.BAD_REQUEST.value(),
+                    ZonedDateTime.now()));
+        }
+        return ResponseEntity.badRequest().body(errorList);
+    }
 }
 
 
