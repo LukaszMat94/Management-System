63ba6af80a9880a1e220c4054b185b073b3e2f37
LukaszMat94 <lmatusik11@gmail.com>
2021-12-18 11:56:55 +0100

    Updated LaptopService
    Visual changes

--- src/main/java/org/matusikl/service/LaptopService.java
+++ src/main/java/org/matusikl/service/LaptopService.java
@@ -6,7 +6,6 @@ import org.matusikl.model.Laptop;
 import org.matusikl.repository.LaptopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
