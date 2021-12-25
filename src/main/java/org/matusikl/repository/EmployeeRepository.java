724fbbe700240bad1ac598865e3a26a656ed9abc
LukaszMat94 <lmatusik11@gmail.com>
2021-12-25 09:44:28 +0100

    Updated EmpRepository
    Override findAll method with entity graph plan

--- src/main/java/org/matusikl/repository/EmployeeRepository.java
+++ src/main/java/org/matusikl/repository/EmployeeRepository.java
@@ -1,10 +1,13 @@
 package org.matusikl.repository;
 
 import org.matusikl.model.Employee;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +17,8 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     @Query("select e from Employee e where e.personalIdentityNumberEmployee = ?1 and e.idEmployee <> ?2")
     Optional<Employee> findByPersonalIdentityNumberEmployeWithOtherID(String idPersonalNumber, Integer idEmployee);
+
+    @Override
+    @EntityGraph(value = "Employee.task", type = EntityGraphType.LOAD)
+    List<Employee> findAll();
 }
